5a2f8c01004186d49ffd96afd2749a70479150ad
kfarleigh <farleik@miamioh.edu>
2021-12-13 12:42:45 -0500

    Add files via upload

---
+++ R/HybridFindR.data.R
@@ -0,0 +1,26 @@
+#' Anolis SNP data for 25 SNPs used to test for signals of differential introgression.
+#'
+#' SNP data for 73 individuals from a study used to infer differential introgression between
+#' Anolis puclhellus and A. krugi. An example data file demonstrating how data should be formatted
+#' for use in HybridFindR.
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage data(HybridFindR)
+#'
+#' @format A data frame that includes sample labels, population assignments, and genotype information for 73 individuals.
+#'
+#' @keywords datasets
+#'
+#' @references Farleigh et al. (in prep)
+#'
+#'
+#' @source Farleigh et al. (in prep)
+#'
+#' @examples
+#' data(HybridFindR)
+#' str(HybridFindR)
+#'
+#'
