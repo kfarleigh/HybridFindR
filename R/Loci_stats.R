1afd443e85fb1d95a0b3bd2d7d1910bdbe45f9e6
kfarleigh <farleik@miamioh.edu>
2021-12-15 16:03:39 -0500

    Add files via upload

--- R/Loci_stats.R
+++ R/Loci_stats.R
@@ -9,11 +9,12 @@
 #' @examples
 #' \dontrun{ Loc_stats <- Loci_stats(Data)}
 Loci_stats <- function(Data) {
-  Data <- Data[,-c(2:3)]
+  Data <- as.data.frame(Data[,-c(2:3)])
+  Loc <- 2:length(Data)
   Homozygous_S1 <- list()
 
   for (i in (2:ncol(Data))) {
-    Homozygous_S1[[i]] <- data.frame(t(Data[which(Data[,..i] == 2),1]))
+    Homozygous_S1[[i]] <- data.frame(t(Data[which(Data[,i] == 2),1]))
   }
   names(Homozygous_S1) <- colnames(Data)
   Homozygous_S1 <- plyr::rbind.fill(Homozygous_S1)
@@ -23,7 +24,7 @@ Loci_stats <- function(Data) {
 
   Heterozygous <- list()
   for (i in (2:ncol(Data))) {
-    Heterozygous[[i]] <- data.frame(t(Data[which(Data[,..i] == 1),1]))
+    Heterozygous[[i]] <- data.frame(t(Data[which(Data[,i] == 1),1]))
   }
   names(Heterozygous) <- colnames(Data)
   Heterozygous <- plyr::rbind.fill(Heterozygous)
@@ -33,7 +34,7 @@ Loci_stats <- function(Data) {
 
   Homozygous_S0 <- list()
   for (i in (2:ncol(Data))) {
-    Homozygous_S0[[i]] <- data.frame(t(Data[which(Data[,..i] == 0),1]))
+    Homozygous_S0[[i]] <- data.frame(t(Data[which(Data[,i] == 0),1]))
   }
   names(Homozygous_S0) <- colnames(Data)
   Homozygous_S0 <- plyr::rbind.fill(Homozygous_S0)
